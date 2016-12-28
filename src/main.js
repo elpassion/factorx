eaad930b5acd9ce5ec87e17c5fb85448c2c58774
Michał Warda <michal.warda@elpassion.pl>
2016-12-28 22:59:41 +0100

    Refactor line range

--- src/main.js
+++ src/main.js
@@ -1,14 +1,18 @@
 // @flow
 
 export function tokenAt (ast: Object, line: number, column: number) {
-  let token = ast.getFirstToken()
-
   function isInTokenColumnRange (token) {
     const {start, end} = token.getLoc()
     return start.column <= column && end.column >= column
   }
 
-  while (token.getLoc().start.line !== line || !isInTokenColumnRange(token)) {
+  function isInTokenLineRange (token) {
+    return token.getLoc().start.line === line
+  }
+
+  let token = ast.getFirstToken()
+
+  while (!isInTokenLineRange(token) || !isInTokenColumnRange(token)) {
     token = token.getNextToken()
   }
   return token
