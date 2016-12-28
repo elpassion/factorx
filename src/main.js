ad246dfe944eb5b78fc9e154e3f460f7d36206d6
Michał Warda <michal.warda@elpassion.pl>
2016-12-28 22:54:18 +0100

    Correct tokenizing when token has more that 1 character

--- src/main.js
+++ src/main.js
@@ -2,7 +2,13 @@
 
 export function tokenAt (ast: Object, line: number, column: number) {
   let token = ast.getFirstToken()
-  while (token.getLoc().start.line !== line || token.getLoc().start.column !== column) {
+
+  function isInTokenColumnRange (token) {
+    const {start, end} = token.getLoc()
+    return start.column <= column && end.column >= column
+  }
+
+  while (token.getLoc().start.line !== line || !isInTokenColumnRange(token)) {
     token = token.getNextToken()
   }
   return token
--- test/test.js
+++ test/test.js
@@ -2,9 +2,20 @@
 import {tokenAt} from '../src/main'
 const cst = require('cst')
 
-test('test', () => {
+test('tokenAt', () => {
   const code = `5 + 2`
   const parsedCode = new cst.Parser().parse(code)
-  expect(tokenAt(parsedCode, 1, 0).value).toEqual(5)
-  expect(tokenAt(parsedCode, 1, 4).value).toEqual(2)
+  expect(tokenAt(parsedCode, 1, 1).value).toEqual(5)
+  expect(tokenAt(parsedCode, 1, 5).value).toEqual(2)
+
+  const code2 = `
+5 + 2
+const name = 5
+  `
+  const parsedCode2 = new cst.Parser().parse(code2)
+  expect(tokenAt(parsedCode2, 2, 1).value).toEqual(5)
+  expect(tokenAt(parsedCode2, 3, 1).value).toEqual('const')
+  expect(tokenAt(parsedCode2, 3, 2).value).toEqual('const')
+  expect(tokenAt(parsedCode2, 3, 7).value).toEqual('name')
+  expect(tokenAt(parsedCode2, 3, 14).value).toEqual(5)
 })
