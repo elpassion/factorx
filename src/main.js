8d45c30f72f478ec9cd8721c90b59ae51ab468e1
Michał Warda <michal.warda@elpassion.pl>
2016-12-28 23:21:08 +0100

    Add simple extract method. Change method to use string instead of ast.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
 // @flow
+const cst = require('cst')
+const {
+  VariableDeclarator,
+  VariableDeclaration,
+  Identifier,
+  NumericLiteral
+} = cst.types
+const {Token} = cst
 
-export function tokenAt (ast: Object, line: number, column: number) {
+export function tokenAt (code: string, line: number, column: number) {
   function isInTokenColumnRange (token) {
     const {start, end} = token.getLoc()
     return start.column <= column && end.column >= column
@@ -10,10 +18,43 @@ export function tokenAt (ast: Object, line: number, column: number) {
     return token.getLoc().start.line === line
   }
 
+  const ast = parse(code)
   let token = ast.getFirstToken()
 
   while (!isInTokenLineRange(token) || !isInTokenColumnRange(token)) {
     token = token.getNextToken()
   }
   return token
+}
+
+export function extract (code: string, line: number, column: number) {
+  const token = tokenAt(code, line, column)
+
+  let VD = new VariableDeclaration([
+    new Token('Keyword', 'const'),
+    new Token('Whitespace', ' '),
+
+    new VariableDeclarator([
+      new Identifier([
+        new Token('Identifier', 'a')
+      ]),
+
+      new Token('Whitespace', ' '),
+      new Token('Punctuator', '='),
+      new Token('Whitespace', ' '),
+
+      // Yeah, a bit convoluted but that's is what we have in original babylon AST
+      new NumericLiteral([
+        new Token('Numeric', token.value)
+      ])
+    ]),
+    new Token('Whitespace', '\n')
+  ])
+  const ast = parse(code)
+  ast.prependChild(VD)
+  return ast
+}
+
+function parse (code) {
+  return new cst.Parser().parse(code)
 }
--- test/test.js
+++ test/test.js
@@ -1,21 +1,25 @@
 // @flow
-import {tokenAt} from '../src/main'
+import {tokenAt, extract} from '../src/main'
 const cst = require('cst')
 
 test('tokenAt', () => {
   const code = `5 + 2`
-  const parsedCode = new cst.Parser().parse(code)
-  expect(tokenAt(parsedCode, 1, 1).value).toEqual(5)
-  expect(tokenAt(parsedCode, 1, 5).value).toEqual(2)
+  expect(tokenAt(code, 1, 1).value).toEqual(5)
+  expect(tokenAt(code, 1, 5).value).toEqual(2)
 
   const code2 = `
 5 + 2
 const name = 5
   `
-  const parsedCode2 = new cst.Parser().parse(code2)
-  expect(tokenAt(parsedCode2, 2, 1).value).toEqual(5)
-  expect(tokenAt(parsedCode2, 3, 1).value).toEqual('const')
-  expect(tokenAt(parsedCode2, 3, 2).value).toEqual('const')
-  expect(tokenAt(parsedCode2, 3, 7).value).toEqual('name')
-  expect(tokenAt(parsedCode2, 3, 14).value).toEqual(5)
+  expect(tokenAt(code2, 2, 1).value).toEqual(5)
+  expect(tokenAt(code2, 3, 1).value).toEqual('const')
+  expect(tokenAt(code2, 3, 2).value).toEqual('const')
+  expect(tokenAt(code2, 3, 7).value).toEqual('name')
+  expect(tokenAt(code2, 3, 14).value).toEqual(5)
+})
+
+test('extract', () => {
+  const code = `5 + 2`
+  expect(extract(code, 1, 1).getSourceCode()).toMatchSnapshot()
+  expect(extract(code, 1, 5).getSourceCode()).toMatchSnapshot()
 })
