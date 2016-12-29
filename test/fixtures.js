66e7ac1de4da1ff9244a510973849a2083111857
Michał Warda <michal.warda@elpassion.pl>
2016-12-29 17:17:44 +0100

    Extract test strings

---
+++ test/fixtures.js
@@ -0,0 +1,7 @@
+// @flow
+export const binaryExpression = `5 + 2`
+export const binaryExpressionString = `'abc' + 'cde'`
+export const multiLineExpression = `
+5 + 2
+const name = 5
+`
--- test/test.js
+++ test/test.js
@@ -1,17 +1,13 @@
 // @flow
 import {tokenAt, extractVariable, parse} from '../src/main'
+import * as exampleCode from './fixtures'
 
 test('tokenAt', () => {
-  const code = `5 + 2`
-  const ast = parse(code)
+  const ast = parse(exampleCode.binaryExpression)
   expect(tokenAt(ast, 1, 1).value).toEqual(5)
   expect(tokenAt(ast, 1, 5).value).toEqual(2)
 
-  const code2 = `
-5 + 2
-const name = 5
-  `
-  const ast2 = parse(code2)
+  const ast2 = parse(exampleCode.multiLineExpression)
   expect(tokenAt(ast2, 2, 1).value).toEqual(5)
   expect(tokenAt(ast2, 3, 1).value).toEqual('const')
   expect(tokenAt(ast2, 3, 2).value).toEqual('const')
@@ -20,12 +16,10 @@ const name = 5
 })
 
 test('extractVariable', () => {
-  const code = `5 + 2`
-  expect(extractVariable(code, 1, 1, 'let', 'number').getSourceCode()).toMatchSnapshot()
-  expect(extractVariable(code, 1, 5, 'let', 'number').getSourceCode()).toMatchSnapshot()
-  const code2 = `'abc' + 'cde'`
-  expect(extractVariable(code2, 1, 1, 'let', 'string').getSourceCode()).toMatchSnapshot()
-  expect(extractVariable(code2, 1, 10, 'const', 'text').getSourceCode()).toMatchSnapshot()
-  expect(extractVariable(code2, 1, 10, 'var', 'varText').getSourceCode()).toMatchSnapshot()
-  expect(extractVariable(code2, 1, 6, 'let', 'addition').getSourceCode()).toMatchSnapshot()
+  expect(extractVariable(exampleCode.binaryExpression, 1, 1, 'let', 'number').getSourceCode()).toMatchSnapshot()
+  expect(extractVariable(exampleCode.binaryExpression, 1, 5, 'let', 'number').getSourceCode()).toMatchSnapshot()
+  expect(extractVariable(exampleCode.binaryExpressionString, 1, 1, 'let', 'string').getSourceCode()).toMatchSnapshot()
+  expect(extractVariable(exampleCode.binaryExpressionString, 1, 10, 'const', 'text').getSourceCode()).toMatchSnapshot()
+  expect(extractVariable(exampleCode.binaryExpressionString, 1, 10, 'var', 'varText').getSourceCode()).toMatchSnapshot()
+  expect(extractVariable(exampleCode.binaryExpressionString, 1, 6, 'let', 'addition').getSourceCode()).toMatchSnapshot()
 })
