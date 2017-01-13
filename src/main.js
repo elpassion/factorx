6dd8c49a368c0b7fd9d40e4b0a85283e3c49c50a
Michał Warda <michal.warda@elpassion.pl>
2017-01-13 10:14:31 +0100

    Remove previous implementation

--- src/main.js
+++ src/main.js
@@ -1,109 +1,68 @@
 // @flow
-const flatten = require('lodash/flatten')
-const cst = require('cst')
-const {VariableDeclarator, VariableDeclaration, Identifier} = cst.types
-const {Token, Parser} = cst
-
-type cursorPosition = { line: number, column: number }
-type location = { cursorStart: cursorPosition, cursorEnd: cursorPosition }
-
-export function extractVariable (
-  code: string,
-  location: location,
-  variableKind: 'let' | 'const' | 'var',
-  variableName: string
-) {
-  const ast = parse(code)
-  const expression = expressionAt(ast, location)
-
-  function createIdentifier () {
-    return new Identifier([new Token('Identifier', variableName)])
-  }
-
-  let VD = new VariableDeclaration([
-    new Token('Keyword', variableKind),
-    new Token('Whitespace', ' '),
-
-    new VariableDeclarator([
-      createIdentifier(),
-      new Token('Whitespace', ' '),
-      new Token('Punctuator', '='),
-      new Token('Whitespace', ' '),
-      expression.cloneElement()
-    ]),
-    new Token('Whitespace', '\n')
-  ])
-
-  let node = expression.parentElement
-  let child = expression
-  while (node.type !== 'Program' && node.type !== 'BlockStatement') {
-    child = node
-    node = node.parentElement
-  }
-  node.insertChildBefore(VD, child)
-  expression.parentElement.replaceChild(createIdentifier(), expression)
-  return ast
-}
-
-function notAnExpressionError () {
-  return new Error('Selection does not form an expression')
-}
-
-export function expressionAt (ast: Object, {cursorStart, cursorEnd}: location) {
-  const matchingExpression = expressionsAt(ast, {cursorStart, cursorEnd}).filter((expression) => {
-    const {start: expressionStart, end: expressionEnd} = expression.getLoc()
-    const isExpressionMatching = (
-      expressionStart.column === cursorStart.column &&
-      expressionStart.line === cursorStart.line + 1 &&
-      expressionEnd.column === cursorEnd.column &&
-      expressionEnd.line === cursorEnd.line + 1
-    )
-    return isExpressionMatching
-  })[0]
-
-  if (matchingExpression) {
-    return matchingExpression
-  } else {
-    throw notAnExpressionError()
-  }
-}
-
-export function expressionsAt (ast: Object, {cursorStart, cursorEnd}: location) {
-  const normalizedLocation = {
-    cursorStart: {line: cursorStart.line + 1, column: cursorStart.column},
-    cursorEnd: {line: cursorEnd.line + 1, column: cursorEnd.column}
-  }
-
-  function matchingExpressions (node, nodesInRange = []) {
-    node.childElements.forEach((child) => {
-      const {start: childStart, end: childEnd} = child.getLoc()
-      let isChildInRange
-      if (childStart.line === childEnd.line) {
-        isChildInRange = (
-          childStart.column <= normalizedLocation.cursorStart.column &&
-          childStart.line === normalizedLocation.cursorStart.line &&
-          childEnd.column >= normalizedLocation.cursorEnd.column &&
-          childEnd.line === normalizedLocation.cursorEnd.line
-        )
-      } else {
-        isChildInRange = (
-          childStart.line <= normalizedLocation.cursorStart.line &&
-          childEnd.line >= normalizedLocation.cursorEnd.line
-        )
-      }
-      if (isChildInRange) {
-        if (child.isExpression) {
-          nodesInRange.push(child)
-        }
-        matchingExpressions(child, nodesInRange)
-      }
-    })
-    return flatten(nodesInRange)
-  }
-
-  return matchingExpressions(ast)
-}
+import * as babylon from 'babylon'
+import traverse from 'babel-traverse'
+import * as t from 'babel-types'
+import {filter} from 'lodash'
 
 export function parse (code: string): Object {
-  return new Parser().parse(code)
+  return babylon.parse(code, {plugins: ['jsx', 'flow']})
+}
+
+function getExpressions (ast: Object) {
+  let expressions = []
+
+  traverse(ast, {
+    enter (path) { if (t.isExpression(path.node)) expressions.push(path.node) }
+  })
+  return expressions
+}
+
+type position = { line: number, column: number }
+type selection = { start: position, end: position }
+
+function normalizePosition (position: position) {
+  return { line: position.line + 1, column: position.column }
+}
+
+function normalizeSelection (selection: selection) {
+  return {
+    start: normalizePosition(selection.start),
+    end: normalizePosition(selection.end)
+  }
+}
+
+export function expressionsAt (ast: Object, selection: selection) {
+  const normalizedSelection = normalizeSelection(selection)
+  const allExpressions = getExpressions(ast)
+
+  function isOneLineExpression (expression) {
+    const expressionPosition = expression.loc
+    return expressionPosition.start.line === expressionPosition.end.line
+  }
+
+  function isOneLineExpressionInRange (expression) {
+    const expressionPosition = expression.loc
+    return (
+      expressionPosition.start.column <= normalizedSelection.start.column &&
+      expressionPosition.start.line === normalizedSelection.start.line &&
+      expressionPosition.end.column >= normalizedSelection.end.column &&
+      expressionPosition.end.line === normalizedSelection.end.line
+    )
+  }
+
+  function isMultiLineExpressionInRange (expression) {
+    const expressionPosition = expression.loc
+    return (
+      expressionPosition.start.line <= normalizedSelection.start.line &&
+      expressionPosition.end.line >= normalizedSelection.end.line
+    )
+  }
+
+  return filter(allExpressions, (expression) => {
+    if (isOneLineExpression(expression)) {
+      return isOneLineExpressionInRange(expression)
+    } else {
+      return isMultiLineExpressionInRange(expression)
+    }
+  })
 }
--- test/test.js
+++ test/test.js
@@ -1,8 +1,6 @@
 // @flow
 import {
-  extractVariable,
   parse,
-  expressionAt,
   expressionsAt
 } from '../src/main'
 import * as exampleCode from './fixtures'
@@ -11,60 +9,30 @@ describe('expressionsAt', () => {
   describe('not during selection', () => {
     describe('at start of an expression', () => {
       test('returns all expressions containing the position', () => {
-        const location = {cursorStart: {line: 0, column: 0}, cursorEnd: {line: 0, column: 0}}
+        const location = {start: {line: 0, column: 0}, end: {line: 0, column: 0}}
         const ast = parse(exampleCode.binaryExpression)
-        expect(expressionsAt(ast, location).map(node => node.getSourceCode())).toEqual(['5 + 2', '5'])
+        expect(expressionsAt(ast, location)).toMatchSnapshot()
 
-        const location2 = {cursorStart: {line: 0, column: 2}, cursorEnd: {line: 0, column: 2}}
+        const location2 = {start: {line: 0, column: 2}, end: {line: 0, column: 2}}
         const ast2 = parse(exampleCode.binaryExpression)
-        expect(expressionsAt(ast2, location2).map(node => node.getSourceCode())).toEqual(['5 + 2'])
+        expect(expressionsAt(ast2, location2)).toMatchSnapshot()
 
-        const location3 = {cursorStart: {line: 0, column: 4}, cursorEnd: {line: 0, column: 4}}
+        const location3 = {start: {line: 0, column: 4}, end: {line: 0, column: 4}}
         const ast3 = parse(exampleCode.binaryExpression)
-        expect(expressionsAt(ast3, location3).map(node => node.getSourceCode())).toEqual(['5 + 2', '2'])
+        expect(expressionsAt(ast3, location3)).toMatchSnapshot()
       })
     })
 
     describe('at end of an expression', () => {
       const ast = parse(exampleCode.binaryExpression)
       test('returns all expressions containing the position', () => {
-        const location = {cursorStart: {line: 0, column: 1}, cursorEnd: {line: 0, column: 1}}
-        expect(expressionsAt(ast, location).map(node => node.getSourceCode())).toEqual(['5 + 2', '5'])
-        const location2 = {cursorStart: {line: 0, column: 3}, cursorEnd: {line: 0, column: 3}}
-        expect(expressionsAt(ast, location2).map(node => node.getSourceCode())).toEqual(['5 + 2'])
-        const location3 = {cursorStart: {line: 0, column: 5}, cursorEnd: {line: 0, column: 5}}
-        expect(expressionsAt(ast, location3).map(node => node.getSourceCode())).toEqual(['5 + 2', '2'])
+        const location = {start: {line: 0, column: 1}, end: {line: 0, column: 1}}
+        expect(expressionsAt(ast, location)).toMatchSnapshot()
+        const location2 = {start: {line: 0, column: 3}, end: {line: 0, column: 3}}
+        expect(expressionsAt(ast, location2)).toMatchSnapshot()
+        const location3 = {start: {line: 0, column: 5}, end: {line: 0, column: 5}}
+        expect(expressionsAt(ast, location3)).toMatchSnapshot()
       })
     })
   })
-})
-
-describe('expressionAt', () => {
-  const ast = parse(exampleCode.binaryExpression)
-  describe('when a specified expression exists', () => {
-    test('returns the expression', () => {
-      const selection = {cursorStart: {line: 0, column: 0}, cursorEnd: {line: 0, column: 1}}
-      expect(expressionAt(ast, selection).getSourceCode()).toEqual('5')
-    })
-  })
-  describe('when a specified expression does not exist', () => {
-    xtest('throws an error', () => {
-      const wrongSelection = {cursorStart: {line: 0, column: 0}, cursorEnd: {line: 0, column: 2}}
-      expect(expressionAt(ast, wrongSelection)).toThrow()
-    })
-  })
-})
-
-test('extractVariable', () => {
-  let location
-  location = {cursorStart: {line: 0, column: 0}, cursorEnd: {line: 0, column: 1}}
-  expect(extractVariable(exampleCode.binaryExpression, location, 'let', 'number').getSourceCode()).toMatchSnapshot()
-  location = {cursorStart: {line: 0, column: 0}, cursorEnd: {line: 0, column: 5}}
-  expect(extractVariable(exampleCode.binaryExpression, location, 'let', 'number').getSourceCode()).toMatchSnapshot()
-  location = {cursorStart: {line: 0, column: 4}, cursorEnd: {line: 0, column: 5}}
-  expect(extractVariable(exampleCode.binaryExpression, location, 'let', 'number').getSourceCode()).toMatchSnapshot()
-  location = {cursorStart: {line: 0, column: 3}, cursorEnd: {line: 0, column: 4}}
-  expect(extractVariable(exampleCode.ifStatement, location, 'let', 'number').getSourceCode()).toMatchSnapshot()
-  location = {cursorStart: {line: 1, column: 2}, cursorEnd: {line: 1, column: 7}}
-  expect(extractVariable(exampleCode.ifStatement, location, 'let', 'number').getSourceCode()).toMatchSnapshot()
 })
