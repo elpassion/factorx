d5877ade7b8870a04d21688bb96a649dbc87370f
Michał Warda <michal.warda@elpassion.pl>
2017-04-26 14:59:17 +0200

    refactor extractMethod

--- src/AstExplorer/ExtractMethodOperation.js
+++ src/AstExplorer/ExtractMethodOperation.js
@@ -24,97 +24,88 @@ export default class ExtractMethodOperation {
     this.selections = selections;
     this.selection = selections[0];
     this.cursorPositions = [];
+    this.variableIdentifier = types.identifier('extracted');
   }
-
-  // addDeclarationToScope = (scope: Object, node: Object) => {
-  //   const variableIdentifier = types.identifier('extracted');
-  //   const functionExpression = types.arrowFunctionExpression([], types.clone(node));
-  //   scope.push({ id: variableIdentifier, init: functionExpression, kind: 'const' });
-  // };
-
-  // replaceSelectionsInScope = (scope: Object) => {
-  //   scope.path.traverse(
-  // {
-  // Expression: (expressionPath) => {
-  // console.log(expressionPath.node);
-  // if (isSelectedExpression(expressionPath)) {
-  // const callExpression = types.callExpression(types.identifier('extracted'), []);
-  // const expressionStatement = types.expressionStatement(callExpression);
-  // expressionPath.replaceWith(expressionStatement);
-  // }
-  // },
-  //     },
-  //   );
-  // };
 
   getPathScope = (path: Object) =>
     (types.isArrowFunctionExpression(path) ? path.scope.parent : path.scope);
 
-  extractMethod = (programPath: Object) => {
+  findDeepestIncludedPath = (programPath: Object) => {
     const state = {};
-    programPath.traverse(
-      {
-        enter: (path) => {
-          if (this.selection.includesPosition(Position.fromNode(path.node))) {
-            state.deepestIncludedPath = path;
-          } else {
-            path.skip();
-          }
-        },
+    programPath.traverse({
+      enter: (path) => {
+        if (this.selection.includesPosition(Position.fromNode(path.node))) {
+          state.deepestIncludedPath = path;
+        } else {
+          path.skip();
+        }
       },
-      state,
-    );
-    if (!state.deepestIncludedPath) {
-      state.deepestIncludedPath = programPath;
-      let nodes = [...state.deepestIncludedPath.node.body];
-      const startNodeIndex = findIndex(state.deepestIncludedPath.node.body, {
-        start: this.selection.start,
-      });
-      const endNodeIndex =
-        findIndex(state.deepestIncludedPath.node.body, {
-          end: this.selection.end,
-        }) + 1;
-      nodes = nodes.slice(startNodeIndex, endNodeIndex);
-      const scope = this.getPathScope(state.deepestIncludedPath);
-      const functionExpression = types.arrowFunctionExpression(
-        [],
-        types.blockStatement(nodes.map(node => types.clone(node))),
-      );
-      const isExactlyTheFirstNode = otherNode =>
-        types.isNodesEquivalent(nodes[0], otherNode) &&
-        Position.fromNode(nodes[0]).equalToPosition(Position.fromNode(otherNode));
+    });
+    return state.deepestIncludedPath;
+  };
 
-      const callExpression = types.callExpression(types.identifier('extracted'), []);
-      const expressionStatement = types.expressionStatement(callExpression);
-      let insertedInvocation = false;
-      if (!state.deepestIncludedPath) throw new ExpressionNotFoundError();
-      state.deepestIncludedPath.traverse({
-        enter: (path) => {
-          if (isExactlyTheFirstNode(path.node)) {
-            if (!insertedInvocation) {
-              path.insertBefore(expressionStatement);
-              insertedInvocation = true;
-            }
-            path.remove();
-            nodes = nodes.slice(1);
+  cloneSelectedNodes = (pathNodes: Array<Object>) => {
+    let nodes = [...pathNodes];
+    const startNodeIndex = findIndex(nodes, {
+      start: this.selection.start,
+    });
+    const endNodeIndex =
+      findIndex(nodes, {
+        end: this.selection.end,
+      }) + 1;
+    nodes = nodes.slice(startNodeIndex, endNodeIndex);
+    return nodes.map(node => types.clone(node));
+  };
+
+  buildExpressionStatement = () => {
+    const callExpression = types.callExpression(this.variableIdentifier, []);
+    return types.expressionStatement(callExpression);
+  };
+
+  removeSelectedNodes = (deepestIncludedPath: Object, clonedNodes: Array<Object>) => {
+    let lastClonedNodeIndex = 0;
+    const isExactlyTheFirstNode = otherNode =>
+      types.isNodesEquivalent(clonedNodes[lastClonedNodeIndex], otherNode) &&
+      Position.fromNode(clonedNodes[lastClonedNodeIndex]).equalToPosition(
+        Position.fromNode(otherNode),
+      );
+    const statement = this.buildExpressionStatement();
+    let insertedInvocation = false;
+    if (!deepestIncludedPath) throw new ExpressionNotFoundError();
+    deepestIncludedPath.traverse({
+      enter: (path) => {
+        if (isExactlyTheFirstNode(path.node)) {
+          if (!insertedInvocation) {
+            path.insertBefore(statement);
+            insertedInvocation = true;
           }
-        },
-      });
-      const variableIdentifier = types.identifier('extracted');
-      scope.push({ id: variableIdentifier, init: functionExpression, kind: 'const' });
+          path.remove();
+          lastClonedNodeIndex += 1;
+        }
+      },
+    });
+  };
+
+  buildVariableDeclaration = (body) => {
+    const functionExpression = types.arrowFunctionExpression([], body);
+    return { id: this.variableIdentifier, init: functionExpression, kind: 'const' };
+  };
+
+  extractMethod = (programPath: Object) => {
+    let deepestIncludedPath = this.findDeepestIncludedPath(programPath);
+    if (deepestIncludedPath) {
+      const scope = this.getPathScope(deepestIncludedPath);
+      const node = types.isExpression(deepestIncludedPath.node)
+        ? deepestIncludedPath.node
+        : types.blockStatement([types.clone(deepestIncludedPath.node)]);
+      scope.push(this.buildVariableDeclaration(types.clone(node)));
+      deepestIncludedPath.replaceWith(this.buildExpressionStatement());
     } else {
-      const scope = this.getPathScope(state.deepestIncludedPath);
-      const variableIdentifier = types.identifier('extracted');
-      let node;
-      if (types.isExpression(state.deepestIncludedPath.node)) node = state.deepestIncludedPath.node;
-      else {
-        node = types.blockStatement([types.clone(state.deepestIncludedPath.node)]);
-      }
-      const functionExpression = types.arrowFunctionExpression([], types.clone(node));
-      scope.push({ id: variableIdentifier, init: functionExpression, kind: 'const' });
-      const callExpression = types.callExpression(types.identifier('extracted'), []);
-      const expressionStatement = types.expressionStatement(callExpression);
-      state.deepestIncludedPath.replaceWith(expressionStatement);
+      deepestIncludedPath = programPath;
+      const scope = this.getPathScope(deepestIncludedPath);
+      const nodes = this.cloneSelectedNodes(deepestIncludedPath.node.body);
+      this.removeSelectedNodes(deepestIncludedPath, nodes);
+      scope.push(this.buildVariableDeclaration(types.blockStatement(nodes)));
     }
   };
 
---
+++ test/extractMethod/multipleStatements.compare.js
@@ -0,0 +1,10 @@
+// input
+/* CS */const a = 5;
+const b = 6;/* CE */
+// output
+const extracted = () => {
+  const a = 5;
+  const b = 6;
+};
+
+extracted();
