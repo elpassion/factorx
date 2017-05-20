9091aabfad9bfd95aabafa07e116cfa508d2cb90
Michał Warda <michalwarda@users.noreply.github.com>
2017-05-20 10:27:50 +0200

    45 automatic member expression extracting (#46)
    * Working for simple case
    
    * Add merging of memberExpressions when extracting
    
    * 0.2.1
    
    * Fix memberExpression when assignment occurs
    
    * Avoid 1 additional recast

--- src/AstExplorer/ExtractVariablesOperation.js
+++ src/AstExplorer/ExtractVariablesOperation.js
@@ -2,7 +2,7 @@
 import recast from 'recast';
 import traverse from 'babel-traverse';
 import * as types from 'babel-types';
-import isEqual from 'lodash/isEqual';
+import { sortBy, isEqual } from 'lodash';
 import Position from '../Position';
 import ExpressionNotFoundError from '../ExpressionNotFoundError';
 import options from './options';
@@ -14,8 +14,9 @@ export default class ExtractVariablesOperation {
   scopeRoad: Array<any>;
   variableType: 'const' | 'let';
   variableIdentifier: Object;
+  variableDeclarator: Object;
   selections: Array<Position>;
-  cursorPositions: Array<Position>;
+  cursorPositions: Array<Position> = [];
 
   constructor(ast: Object, variableType: 'const' | 'let' = 'let', selections: Array<Position>) {
     this.ast = ast;
@@ -54,9 +55,26 @@ export default class ExtractVariablesOperation {
     });
   }
 
-  addDeclarationToScope(scope: Object, node: Object) {
-    this.variableIdentifier = scope.generateUidIdentifierBasedOnNode(node.id);
-    scope.push({ id: this.variableIdentifier, init: node, kind: this.variableType });
+  addDeclarationToScope(scope: Object, path: Object) {
+    if (types.isMemberExpression(path)) {
+      const { node } = path;
+      this.variableIdentifier = scope.generateUidIdentifierBasedOnNode(node.id);
+      scope.push({ id: this.variableIdentifier, init: node, kind: this.variableType });
+      const variableDeclarator = types.variableDeclarator(
+        types.objectPattern([
+          types.objectProperty(types.clone(node.property), types.clone(node.property), false, true),
+        ]),
+        node.object,
+      );
+
+      scope.bindings[this.variableIdentifier.name].path.replaceWith(variableDeclarator);
+      this.variableDeclarator = variableDeclarator;
+      this.variableIdentifier = node.property;
+    } else {
+      const { node } = path;
+      this.variableIdentifier = scope.generateUidIdentifierBasedOnNode(node.id);
+      scope.push({ id: this.variableIdentifier, init: node, kind: this.variableType });
+    }
   }
 
   // eslint-disable-next-line class-methods-use-this
@@ -86,7 +104,7 @@ export default class ExtractVariablesOperation {
         if (!node.visited && firstSelection.includes(Position.fromNode(node))) {
           const scope = getPathScope(path);
           node.visited = true;
-          this.addDeclarationToScope(scope, node);
+          this.addDeclarationToScope(scope, path);
           this.replaceSelectionsInScope(scope);
           this.saveScopeRoad(scope);
           if (this.isAllSelectionsExtracted()) programPath.stop();
@@ -103,19 +121,44 @@ export default class ExtractVariablesOperation {
       {
         VariableDeclarator: (declaratorPath, state) => {
           if (
-            isEqual(this.findScopeRoad(declaratorPath.scope), this.scopeRoad) &&
-            declaratorPath.node.id.name === this.variableIdentifier.name
+            (isEqual(this.findScopeRoad(declaratorPath.scope), this.scopeRoad) &&
+              declaratorPath.node.id.name === this.variableIdentifier.name) ||
+            (types.isObjectPattern(declaratorPath.node.id) &&
+              declaratorPath.node.id.properties
+                .map(property => property.value.name)
+                .includes(this.variableIdentifier.name))
           ) {
             const { scope } = declaratorPath;
             const binding = scope.bindings[this.variableIdentifier.name];
             const declarationPath = declaratorPath.parentPath;
-            const firstReferencePath = binding.referencePaths[0];
+            let referencePaths = [];
+            scope.path.traverse({
+              AssignmentExpression: (assignmentPath) => {
+                if (assignmentPath.node.left.name === this.variableIdentifier.name) {
+                  assignmentPath.traverse({
+                    Identifier: (identifierPath) => {
+                      if (
+                        identifierPath.key === 'left' &&
+                        identifierPath.parentPath === assignmentPath
+                      ) {
+                        referencePaths.push(identifierPath);
+                      }
+                    },
+                  });
+                }
+              },
+            });
+            referencePaths = referencePaths.concat(
+              binding.referencePaths.filter(refPath => refPath.node !== binding.identifier),
+            );
+            referencePaths = sortBy(referencePaths, [path => path.node.start]);
+            const firstReferencePath = referencePaths[0];
             const firstReferencePathParentStatement = firstReferencePath.find(
               path =>
                 types.isStatement(path) &&
                 types.isNodesEquivalent(path.scope.path.node, scope.path.node),
             );
-            //eslint-disable-next-line
+            // eslint-disable-next-line
             state.firstReferencePathParentStatement = firstReferencePathParentStatement;
             //eslint-disable-next-line
             state.replacement = types.clone(declarationPath.node);
@@ -127,6 +170,36 @@ export default class ExtractVariablesOperation {
       traversalState,
     );
     traversalState.firstReferencePathParentStatement.insertBefore(traversalState.replacement);
+  };
+
+  mergeDeclarations = (programPath: Object) => {
+    programPath.traverse({
+      VariableDeclarator: (declaratorPath) => {
+        if (
+          isEqual(this.findScopeRoad(declaratorPath.scope), this.scopeRoad) &&
+          (types.isObjectPattern(declaratorPath.node.id) &&
+            declaratorPath.node.id.properties
+              .map(property => property.value.name)
+              .includes(this.variableIdentifier.name))
+        ) {
+          const { scope } = declaratorPath;
+          const binding = scope.bindings[this.variableIdentifier.name];
+          const bindingsWithSameInit = Object.values(scope.bindings).filter((nextBinding: any) =>
+            types.isNodesEquivalent(binding.path.node.init, nextBinding.path.node.init),
+          );
+          const highestBinding = (bindingsWithSameInit[0]: any);
+          const clonedProperty = types.clone(binding.path.node.id.properties[0]);
+          if (binding !== highestBinding) {
+            binding.path.remove();
+            highestBinding.path.traverse({
+              ObjectPattern: (opPath) => {
+                opPath.pushContainer('properties', clonedProperty);
+              },
+            });
+          }
+        }
+      },
+    });
   };
 
   setCursorPosition = (programPath: Object) => {
@@ -141,6 +214,35 @@ export default class ExtractVariablesOperation {
           const referencePaths = binding.referencePaths.map(path => Position.fromNode(path.node));
           this.cursorPositions = [Position.fromNode(binding.path.node.id), ...referencePaths];
           programPath.stop();
+        } else if (
+          types.isObjectPattern(declaratorPath.node.id) &&
+          declaratorPath.node.id.properties
+            .map(property => property.value.name)
+            .includes(this.variableIdentifier.name)
+        ) {
+          const { scope } = declaratorPath;
+          const binding = scope.bindings[this.variableIdentifier.name];
+          let referencePaths = [];
+          scope.path.traverse({
+            AssignmentExpression: (assignmentPath) => {
+              if (assignmentPath.node.left.name === this.variableIdentifier.name) {
+                assignmentPath.traverse({
+                  Identifier: (identifierPath) => {
+                    if (
+                      identifierPath.key === 'left' &&
+                      identifierPath.parentPath === assignmentPath
+                    ) {
+                      referencePaths.push(identifierPath);
+                    }
+                  },
+                });
+              }
+            },
+          });
+          referencePaths = referencePaths.concat(binding.referencePaths);
+          referencePaths = sortBy(referencePaths, [path => path.node.start]);
+          const referencePositions = referencePaths.map(path => Position.fromNode(path.node));
+          this.cursorPositions = [...referencePositions];
         }
       },
     });
@@ -149,18 +251,21 @@ export default class ExtractVariablesOperation {
   start(): { ast: Object, result: { code: string, cursorPositions: Array<Position> } } {
     this.transform(this.extractVariable);
     this.transform(this.moveDeclaration);
-    this.transform(this.setCursorPosition);
+    this.transform(this.mergeDeclarations);
+    this.transform(this.setCursorPosition, { recast: false });
 
     return { ast: this.ast, result: { code: this.code, cursorPositions: this.cursorPositions } };
   }
 
-  transform(transformation: Function) {
+  transform(transformation: Function, transformOptions: { recast: boolean } = { recast: true }) {
     traverse(this.ast, {
       Program(programPath) {
         transformation(programPath);
       },
     });
-    this.code = recast.print(this.ast).code;
-    this.ast = recast.parse(this.code, options);
+    if (transformOptions.recast) {
+      this.code = recast.print(this.ast).code;
+      this.ast = recast.parse(this.code, options);
+    }
   }
 }
---
+++ test/extractVariable/memberExpressionAutoMerge.compare.js
@@ -0,0 +1,12 @@
+// input
+const { e } = a.b;
+/* CS */a.b.c/* CE */
+/* CS */a.b.c/* CE */.d
+// output
+const {
+  e,
+  /* CS */c/* CE */
+} = a.b;
+
+/* CS */c/* CE */
+/* CS */c/* CE */.d
